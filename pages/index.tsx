87730099eef26b1f0b464681b42d752740f83df0
Matheus Filipe <mfcr031@gmail.com>
2021-05-07 17:57:20 -0300

    clear

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link'
 
 const IndexPage = () => (
   <div>
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
   </div>
 )
 
